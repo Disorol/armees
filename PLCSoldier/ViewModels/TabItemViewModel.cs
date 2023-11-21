c511f5fb7b9df4c8814a981d8b7d6d695b2aa050
Disorol <49810312+Disorol@users.noreply.github.com>
2023-11-21 10:43:51 +0500

    TabItemViewModels are unified.

--- PLCSoldier/ViewModels/TabItemViewModel.cs
+++ PLCSoldier/ViewModels/TabItemViewModel.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PLCSoldier.Interfaces;
 
 namespace PLCSoldier.ViewModels
 {
-    public class CentralViewModel : ITabItem
+    public class TabItemViewModel
     {
         public string IdentificationName { get; set; }
         public string Header { get; set; }
