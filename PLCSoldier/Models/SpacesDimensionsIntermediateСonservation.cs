bddbb53b09c9f37e8f6a66c01d9c6363f9290e2f
Disorol <49810312+Disorol@users.noreply.github.com>
2023-11-29 09:41:39 +0500

    More bug fixes.

--- PLCSoldier/Models/SpacesDimensionsIntermediateСonservation.cs
+++ PLCSoldier/Models/SpacesDimensionsIntermediateСonservation.cs
@@ -7,12 +7,61 @@ using System.Threading.Tasks;
 
 namespace PLCSoldier.Models
 {
+    /*
+    The dimensions of spaces are saved here in cases
+    when all tabs of one space are closed and it disappears from view.
+
+    The sizes of only those spaces that are set in pixels are saved
+    (according to the conceived logic, all neighboring spaces for the
+    presented spaces are set by a star).
+
+    Also, the "setters" take into
+    account cases of attempts to record one star or zero pixels.
+    Such cases may occur during the closure of the adjacent space.
+
+    The class is a singleton because it doesn't make sense to store multiple instances.
+    */
     public class SpacesDimensionsIntermediateСonservation
     {
-        public GridLength LeftSpaceWidth { get; set; }
-        public GridLength LeftBottomSpaceHeight { get; set; }
-        public GridLength BottomSpaceHeight { get; set; }
-        public GridLength FarRightSpaceWidth { get; set; }
+        private GridLength _LeftSpaceWidth;
+        public GridLength LeftSpaceWidth
+        {
+            get => _LeftSpaceWidth;
+            set
+            {
+                if (value != new GridLength(1, GridUnitType.Star) && value != new GridLength(0, GridUnitType.Pixel)) _LeftSpaceWidth = value;
+            }
+        }
+
+        private GridLength _LeftBottomSpaceHeight;
+        public GridLength LeftBottomSpaceHeight
+        {
+            get => _LeftBottomSpaceHeight;
+            set
+            {
+                if (value != new GridLength(1, GridUnitType.Star) && value != new GridLength(0, GridUnitType.Pixel)) _LeftBottomSpaceHeight = value;
+            }
+        }
+
+        private GridLength _BottomSpaceHeight;
+        public GridLength BottomSpaceHeight
+        {
+            get => _BottomSpaceHeight;
+            set
+            {
+                if (value != new GridLength(1, GridUnitType.Star) && value != new GridLength(0, GridUnitType.Pixel)) _BottomSpaceHeight = value;
+            }
+        }
+
+        private GridLength _FarRightSpaceWidth;
+        public GridLength FarRightSpaceWidth
+        {
+            get => _FarRightSpaceWidth;
+            set
+            {
+                if (value != new GridLength(1, GridUnitType.Star) && value != new GridLength(0, GridUnitType.Pixel)) _FarRightSpaceWidth = value;
+            }
+        }
 
         private SpacesDimensionsIntermediateСonservation() { }
 
