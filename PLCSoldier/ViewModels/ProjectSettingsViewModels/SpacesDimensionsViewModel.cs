0dbb5c373cbe517140e9b7747c49468bf9dce3b0
Disorol <49810312+Disorol@users.noreply.github.com>
2023-11-29 09:13:35 +0500

    The logic of the class storing intermediate dimensions of spaces has been changed. Added a comment.

--- PLCSoldier/ViewModels/ProjectSettingsViewModels/SpacesDimensionsViewModel.cs
+++ PLCSoldier/ViewModels/ProjectSettingsViewModels/SpacesDimensionsViewModel.cs
@@ -9,93 +9,73 @@ using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PLCSoldier.ViewModels.ProjectSettingsViewModels
+namespace PLCSoldier.Models
 {
-    public class SpacesDimensionsViewModel : ViewModelBase
+    /*
+        The dimensions of spaces are saved here in cases 
+        when all tabs of one space are closed and it disappears from view.
+
+        The sizes of only those spaces that are set in pixels are saved 
+        (according to the conceived logic, all neighboring spaces for the 
+        presented spaces are set by a star). 
+        
+        Also, the "setters" take into 
+        account cases of attempts to record one star or zero pixels. 
+        Such cases may occur during the closure of the adjacent space.
+
+        The class is a singleton because it doesn't make sense to store multiple instances.
+    */
+    public class SpacesDimensionsIntermediateСonservation
     {
         private GridLength _LeftSpaceWidth;
         public GridLength LeftSpaceWidth
         {
             get => _LeftSpaceWidth;
-            set => this.RaiseAndSetIfChanged(ref _LeftSpaceWidth, value);
-        }
-
-        private GridLength _RightSpaceWidth;
-        public GridLength RightSpaceWidth
-        {
-            get => _RightSpaceWidth;
-            set => this.RaiseAndSetIfChanged(ref _RightSpaceWidth, value);
-        }
-
-        private GridLength _LeftUpperSpaceHeight;
-        public GridLength LeftUpperSpaceHeight
-        {
-            get => _LeftUpperSpaceHeight;
-            set => this.RaiseAndSetIfChanged(ref _LeftUpperSpaceHeight, value);
+            set
+            {
+                if (value != new GridLength(1, GridUnitType.Star) && value != new GridLength(0, GridUnitType.Pixel)) _LeftSpaceWidth = value;
+            }
         }
 
         private GridLength _LeftBottomSpaceHeight;
         public GridLength LeftBottomSpaceHeight
         {
             get => _LeftBottomSpaceHeight;
-            set => this.RaiseAndSetIfChanged(ref _LeftBottomSpaceHeight, value);
-        }
-
-        private GridLength _CentralAndFarRightSpacesHeight;
-        public GridLength CentralAndFarRightSpacesHeight
-        {
-            get => _CentralAndFarRightSpacesHeight;
-            set => this.RaiseAndSetIfChanged(ref _CentralAndFarRightSpacesHeight, value);
+            set
+            {
+                if (value != new GridLength(1, GridUnitType.Star) && value != new GridLength(0, GridUnitType.Pixel)) _LeftBottomSpaceHeight = value;
+            }
         }
 
         private GridLength _BottomSpaceHeight;
         public GridLength BottomSpaceHeight
         {
             get => _BottomSpaceHeight;
-            set => this.RaiseAndSetIfChanged(ref _BottomSpaceHeight, value);
-        }
-
-        private GridLength _CentralSpaceWidth;
-        public GridLength CentralSpaceWidth
-        {
-            get => _CentralSpaceWidth;
-            set => this.RaiseAndSetIfChanged(ref _CentralSpaceWidth, value);
+            set
+            {
+                if (value != new GridLength(1, GridUnitType.Star) && value != new GridLength(0, GridUnitType.Pixel)) _BottomSpaceHeight = value;
+            }
         }
 
         private GridLength _FarRightSpaceWidth;
         public GridLength FarRightSpaceWidth
         {
             get => _FarRightSpaceWidth;
-            set => this.RaiseAndSetIfChanged(ref _FarRightSpaceWidth, value);
+            set
+            {
+                if (value != new GridLength(1, GridUnitType.Star) && value != new GridLength(0, GridUnitType.Pixel)) _FarRightSpaceWidth = value;
+            }
         }
 
-        // Setting default values
-        public SpacesDimensionsViewModel() 
+        private SpacesDimensionsIntermediateСonservation() { }
+
+        private static SpacesDimensionsIntermediateСonservation instance;
+
+        public static SpacesDimensionsIntermediateСonservation getInstance()
         {
-            LeftSpaceWidth = new GridLength(300, GridUnitType.Pixel);
-            RightSpaceWidth = new GridLength(220, GridUnitType.Star);
-
-            LeftUpperSpaceHeight = new GridLength(220, GridUnitType.Star);
-            LeftBottomSpaceHeight = new GridLength(220, GridUnitType.Pixel);
-
-            CentralAndFarRightSpacesHeight = new GridLength(220, GridUnitType.Star);
-            BottomSpaceHeight = new GridLength(150, GridUnitType.Pixel);
-
-            CentralSpaceWidth = new GridLength(220, GridUnitType.Star);
-            FarRightSpaceWidth = new GridLength(150, GridUnitType.Pixel);
-        }
-
-        // Setting custom values
-        public SpacesDimensionsViewModel(GridLength leftSpaceWidth, GridLength rightSpaceWidth, GridLength leftUpperSpaceHeight, GridLength leftBottomSpaceHeight, GridLength centralAndFarRightSpaceHeight, GridLength bottomSpaceHeight, GridLength centralSpaceWidth, GridLength farRightSpaceWidth)
-        {
-            LeftSpaceWidth = leftSpaceWidth;
-            RightSpaceWidth = rightSpaceWidth;
-            LeftUpperSpaceHeight = leftUpperSpaceHeight;
-            LeftBottomSpaceHeight = leftBottomSpaceHeight;
-            CentralAndFarRightSpacesHeight = centralAndFarRightSpaceHeight;
-            BottomSpaceHeight = bottomSpaceHeight;
-            CentralSpaceWidth = centralSpaceWidth;
-            FarRightSpaceWidth = farRightSpaceWidth;
+            if (instance == null)
+                instance = new SpacesDimensionsIntermediateСonservation();
+            return instance;
         }
     }
 }
