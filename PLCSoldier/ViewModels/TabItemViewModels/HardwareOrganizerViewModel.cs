b972a919a4afad2c8161b020060ff9da393fd9a7
Disorol <49810312+Disorol@users.noreply.github.com>
2023-12-01 10:27:10 +0500

    Bag fixes...

--- PLCSoldier/ViewModels/TabItemViewModels/HardwareOrganizerViewModel.cs
+++ PLCSoldier/ViewModels/TabItemViewModels/HardwareOrganizerViewModel.cs
@@ -11,7 +11,7 @@ namespace PLCSoldier.ViewModels.TabItemViewModels
     /*
         The view model that defines the content for the tab of the same name. 
     */
-    public class WatchViewModel : ViewModelBase
+    public class HardwareOrganizerViewModel : ViewModelBase
     {
         private string _SomeText;
         public string SomeText
